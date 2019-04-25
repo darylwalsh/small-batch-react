47af9e12b8e39d8e2913f10deeb0d3c8e7d900fc
Daryl <daryl@blpc.us>
2019-04-25 10:41:35 -0400

    setup: create dockerfile and docker-compose

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,6 @@
 # services/users/project/__init__.py
 
-
+import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
@@ -11,7 +11,8 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')  # new
+app.config.from_object(app_settings)      # new
 
 class UsersPing(Resource):
     def get(self):
