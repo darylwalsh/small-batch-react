c456a03a8ed055221f695f3ed5b7402722052107
daryl-walsh <gitopsreact@blpc.us>
2019-05-08 07:19:45 -0400

    feat: created skeleton react app, added first components

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -25,7 +25,7 @@ class TestDevelopmentConfig(TestCase):
             app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_URL')
         )
-
+        self.assertTrue(app.config['DEBUG_TB_ENABLED']) 
 
 class TestTestingConfig(TestCase):
     def create_app(self):
@@ -40,7 +40,7 @@ class TestTestingConfig(TestCase):
             app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.environ.get('DATABASE_TEST_URL')
         )
-
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 class TestProductionConfig(TestCase):
     def create_app(self):
@@ -50,7 +50,7 @@ class TestProductionConfig(TestCase):
     def test_app_is_production(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['TESTING'])
-
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 if __name__ == '__main__':
     unittest.main()
