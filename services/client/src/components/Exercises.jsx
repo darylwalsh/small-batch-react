c7c5efba121e841219051235b5b9761bda8ce44f
Daryl <daryl@blpc.us>
2019-09-24 03:34:34 -0400

    feat(exercises_client): add exercises client code
    added react code for exercises views

--- services/client/src/components/Exercises.jsx
+++ services/client/src/components/Exercises.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import AceEditor from 'react-ace'
 import 'brace/mode/python'
 import 'brace/theme/solarized_dark'
+import axios from 'axios'
 
 class Exercises extends Component {
   constructor(props) {
@@ -10,48 +11,62 @@ class Exercises extends Component {
       exercises: [],
       editor: {
         value: '# Enter your code here.',
+        button: { isDisabled: false },
+        showGrading: false,
+        showCorrect: false,
+        showIncorrect: false,
       },
     }
     this.onChange = this.onChange.bind(this)
     this.submitExercise = this.submitExercise.bind(this)
   }
-  getExercises() {
-    const exercises = [
-      {
-        id: 0,
-        body: `Define a function called sum that takes
-      two integers as arguments and returns their sum.`,
-      },
-      {
-        id: 1,
-        body: `Define a function called reverse that takes a string
-      as an argument and returns the string in reversed order.`,
-      },
-      {
-        id: 2,
-        body: `Define a function called factorial that takes a random
-      number as an argument and then returns the factorial of that
-      given number.`,
-      },
-    ]
-    this.setState({ exercises: exercises })
-  }
-
-  onChange(value) {
-    this.setState({
-      editor: {
-        value: value,
-      },
-    })
-  }
-  submitExercise(event) {
-    event.preventDefault()
-    console.log(this.state.editor.value)
-  }
   componentDidMount() {
     this.getExercises()
   }
-
+  getExercises() {
+    axios
+      .get(`${process.env.REACT_APP_EXERCISES_SERVICE_URL}/exercises`)
+      .then(res => {
+        this.setState({ exercises: res.data.data.exercises })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  onChange(value) {
+    const newState = this.state.editor
+    newState.value = value
+    this.setState(newState)
+  }
+  submitExercise(event) {
+    event.preventDefault()
+    const newState = this.state.editor
+    newState.showGrading = true
+    newState.showCorrect = false
+    newState.showIncorrect = false
+    newState.button.isDisabled = true
+    this.setState(newState)
+    const data = { answer: this.state.editor.value }
+    const url = process.env.REACT_APP_API_GATEWAY_URL
+    axios
+      .post(url, data)
+      .then(res => {
+        newState.showGrading = false
+        newState.button.isDisabled = false
+        if (res.data) {
+          newState.showCorrect = true
+        }
+        if (!res.data) {
+          newState.showIncorrect = true
+        }
+        this.setState(newState)
+      })
+      .catch(err => {
+        newState.showGrading = false
+        newState.button.isDisabled = false
+        console.log(err)
+      })
+  }
   render() {
     return (
       <div>
@@ -71,7 +86,6 @@ class Exercises extends Component {
               theme="solarized_dark"
               name={this.state.exercises[0].id.toString()}
               onLoad={this.onLoad}
-              onChange={this.onChange}
               fontSize={14}
               height={'175px'}
               showPrintMargin={true}
@@ -84,13 +98,41 @@ class Exercises extends Component {
               editorProps={{
                 $blockScrolling: Infinity,
               }}
+              onChange={this.onChange}
             />
             {this.props.isAuthenticated && (
-              <button
-                className="button is-primary"
-                onClick={this.submitExercise}>
-                Run Code
-              </button>
+              <div>
+                <button
+                  className="button is-primary"
+                  onClick={this.submitExercise}
+                  disabled={this.state.editor.button.isDisabled}>
+                  Run Code
+                </button>
+                {this.state.editor.showGrading && (
+                  <h5 className="title is-5">
+                    <span className="icon is-large">
+                      <i className="fas fa-spinner fa-pulse" />
+                    </span>
+                    <span className="grade-text">Grading...</span>
+                  </h5>
+                )}
+                {this.state.editor.showCorrect && (
+                  <h5 className="title is-5">
+                    <span className="icon is-large">
+                      <i className="fas fa-check" />
+                    </span>
+                    <span className="grade-text">Correct!</span>
+                  </h5>
+                )}
+                {this.state.editor.showIncorrect && (
+                  <h5 className="title is-5">
+                    <span className="icon is-large">
+                      <i className="fas fa-times" />
+                    </span>
+                    <span className="grade-text">Incorrect!</span>
+                  </h5>
+                )}
+              </div>
             )}
             <br />
             <br />
--- test.sh
+++ test.sh
@@ -37,7 +37,7 @@ client() {
 e2e() {
   docker-compose -f docker-compose-prod.yml up -d --build
   docker-compose -f docker-compose-prod.yml exec users python manage.py recreate_db
-  ./node_modules/.bin/cypress run --config baseUrl=http://localhost
+  ./node_modules/.bin/cypress run --config baseUrl=http://localhost --env REACT_APP_API_GATEWAY_URL=$REACT_APP_API_GATEWAY_URL
   inspect $? e2e
   docker-compose -f docker-compose-prod.yml down
 }
