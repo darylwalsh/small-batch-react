bfcde81bbce5dce4c641bc2301710909b48786e9
Daryl <daryl@blpc.us>
2019-09-25 02:53:46 -0400

    ci(aws_ecs): update aws ecs for exercises
    updated travis and aws ecs

--- cypress/integration/swagger.spec.js
+++ cypress/integration/swagger.spec.js
@@ -1,14 +1,16 @@
-it('should display the swagger docs correctly', () => {
-  cy.visit('/')
-    .get('.navbar-burger')
-    .click()
-    .get('a')
-    .contains('Swagger')
-    .click()
+describe('Swagger', () => {
+  it('should display the swagger docs correctly', () => {
+    cy.visit('/')
+      .get('.navbar-burger')
+      .click()
+      .get('a')
+      .contains('Swagger')
+      .click()
 
-  cy.get('select > option').then(el => {
-    cy.location().then(loc => {
-      expect(el.text()).to.contain(loc.hostname)
+    cy.get('select > option').then(el => {
+      expect(el.text()).to.contain(
+        Cypress.env('sbr-staging-alb-1020513120.us-west-1.elb.amazonaws.com')
+      )
     })
   })
 })
