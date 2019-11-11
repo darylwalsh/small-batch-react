8d3842f64d47f821cea2c9ebe40a7fedfa4617c7
Daryl <daryl@blpc.us>
2019-11-11 11:04:04 -0500

    ci(docker_push): update docker push for prod
    updated docker push prod

--- docker-deploy-prod.sh
+++ docker-deploy-prod.sh
@@ -53,6 +53,14 @@ then
       register_definition
       update_service  
 
+      # exercises
+      service="testdriven-exercises-prod-service"
+      template="ecs_exercises_prod_taskdefinition.json"
+      task_template=$(cat "ecs/$template")
+      task_def=$(printf "$task_template" $AWS_ACCOUNT_ID "tbd")
+      echo "$task_def"
+      register_definition
+
       # swagger
       service="sbr-swagger-prod-service"  
       template="ecs_swagger_prod_taskdefinition.json"
